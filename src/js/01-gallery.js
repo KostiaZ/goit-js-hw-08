c0ff1e2bf66a8dcea47902221d7aac33329ad4c0
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2022-12-26 16:01:45 +0200

    add file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,24 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(SimpleLightbox);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', markup);
+gallery.style.listStyle = 'none';
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
