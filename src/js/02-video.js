413a17b142079dc12569b6526c0ff7af0c5b459c
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2022-12-26 17:12:13 +0200

    finished second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,33 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import { throttle } from 'lodash';
+
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  const stringifyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, stringifyData);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function resumePlayback() {
+  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+    return;
+  }
+  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
+  if (paused) {
+    player
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+}
+resumePlayback();
